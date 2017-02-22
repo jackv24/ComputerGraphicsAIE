5833c3d521c9d5294788ef7b7bf45fbb853ff88f
Jack Vine <jackv949@gmail.com>
2017-02-23 09:00:56 +1030

    Added soulspear model

--- MyApplication/MyApplication.cpp
+++ MyApplication/MyApplication.cpp
@@ -58,12 +58,10 @@ bool MyApplication::startup()
 	animatedModel.LoadTexture("models/Pyro/Pyro_N.tga", 1);
 	animatedModel.LoadTexture("models/Pyro/Pyro_S.tga", 2);
 
-	staticModel.Load("models/sphere.obj");
-	staticModel.LoadTexture("textures/earth_diffuse.jpg", 0);
-	staticModel.LoadTexture("textures/earth_normal.jpg", 1);
-	staticModel.LoadTexture("textures/earth_specular.jpg", 2);
-
-	glDisable(GL_CULL_FACE);
+	staticModel.Load("models/soulspear.obj");
+	staticModel.LoadTexture("textures/soulspear_diffuse.tga", 0);
+	staticModel.LoadTexture("textures/soulspear_normal.tga", 1);
+	staticModel.LoadTexture("textures/soulspear_specular.tga", 2);
 
 	return true;
 }
@@ -146,6 +144,6 @@ void MyApplication::draw()
 		unsigned int lightUniform = glGetUniformLocation(m_staticShaderID, "light");
 		glUniform4f(lightUniform, sin(m_time), 0.4f, cos(m_time), 1);
 
-		staticModel.Draw(glm::translate(vec3(0, -2.5f, 0)) * glm::scale(vec3(10, 10, 10)), cameraMatrix, m_staticShaderID);
+		staticModel.Draw(glm::translate(vec3(5, 0, 0)) * glm::scale(vec3(2)), cameraMatrix, m_staticShaderID);
 	}
 }
